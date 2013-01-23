6a86101be78058c2c31917340b7d949be45057c2
Alexander Mahan <alexmahan@gmail.com>
2013-01-23 13:55:06 -0800

    adding html and plugin js

---
+++ instagrabber.js
@@ -0,0 +1,30 @@
+(function($){
+
+	$.fn.extend({  
+	 
+    instaGrabber: function(options) {
+      var defaults = {
+        postID: [],
+        imageSize: 'm',
+        container: 'li'
+      }
+           
+      var options =  $.extend(defaults, options);
+
+      return this.each(function() {
+        var o = options;
+        		obj = $(this);
+        		size = o.imageSize;
+        		wrapper = o.container;
+        
+        $.each(o.postID, function(index,value) {
+			  	$(obj).append('<'+wrapper+'><a href="http://instagr.am/p/'+value+'"><img src="http://instagr.am/p/'+value+'/media/?size='+size+'"></a></'+wrapper+'>');
+			  });
+          
+      });
+      
+    }
+    
+  });
+  
+})(jQuery);
