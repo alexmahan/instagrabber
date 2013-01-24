aeedb290aad4696da2748c2e0f7c43b034b3d124
Alexander Mahan <alexmahan@gmail.com>
2013-01-23 16:31:54 -0800

    fixing tabs

--- instagrabber.js
+++ instagrabber.js
@@ -1,23 +1,28 @@
 (function ($) {
 	$.fn.extend({
 		instaGrabber: function (options) {
-			var defaults = {
+		
+			var settings = {
 					postID: [],
 					imageSize: 'm',
 					container: 'li'
 			}
 			
-			var options =  $.extend(defaults, options);
+			var options =  $.extend(settings, options);
 			
 			return this.each(function () {
+			
 				var o = options;
 					obj = $(this);
 					size = o.imageSize;
 					wrapper = o.container;
+					
 				$.each(o.postID, function (index,value) {
 					$(obj).append('<'+wrapper+'><a href="http://instagr.am/p/'+value+'"><img src="http://instagr.am/p/'+value+'/media/?size='+size+'"></a></'+wrapper+'>');
 				});
+				
 			});
+			
 		}
 	});
 })(jQuery);
